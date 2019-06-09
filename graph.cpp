eb0332c8b02e25ee12215879ea3ea948a244fdae
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2019-06-09 10:30:36 +0200

    Implemented Graph class

--- graph.cpp
+++ graph.cpp
@@ -1,3 +1,70 @@
 #include "graph.hpp"
 
-Graph::Graph(const std::string &filename) {}
+Graph::Graph() : _nodes({{}}) {}
+
+Graph::Graph(const std::string &filename) {
+  std::ifstream infile;
+  infile.open(filename);
+  if (infile.is_open()) {
+    // ignore NODES: line
+    infile.ignore(256, '\n');
+    while (infile.peek() != 'E') {
+      NodeIndex idx;
+      double x, y;
+      infile >> idx >> x >> y;
+      NodePtr n = std::make_shared<Node>(x, y);
+      _nodes.insert(std::make_pair(idx, n));
+    }
+    // skip EDGES: line
+    infile.ignore(256, '\n');
+    while (infile.good()) {
+      NodeIndex a, b;
+      infile >> a >> b;
+      _nodes.at(a)->addNeighbour(_nodes.at(b));
+      _nodes.at(b)->addNeighbour(_nodes.at(a));
+    }
+  } else {
+    throw std::runtime_error("Could not read " + filename + "\n");
+  }
+}
+
+Graph::~Graph() noexcept {}
+
+Graph &Graph::operator=(const Graph &rhs) {
+  // Check for self-assignment!
+  if (this == &rhs)
+    return *this;
+  for (auto &pair : rhs._nodes) {
+    // deep copy Node
+    NodePtr n = std::make_shared<Node>(*(std::get<1>(pair)));
+    _nodes.insert(std::make_pair(std::get<0>(pair), n));
+  }
+
+  return *this;
+}
+
+DistanceResult Graph::_edgeLength(const NodeIndex i, const NodeIndex j) {
+  return _nodes.at(i)->getDistancetoNeighbour(_nodes.at(j));
+}
+
+double Graph::edgeLength(const NodeIndex i, const NodeIndex j) {
+  return _edgeLength(i, j).distance;
+}
+
+double Graph::pathLength(const std::vector<NodeIndex> &path) {
+  DistanceResult pathLengthResult(true, 0.0);
+  for (auto it = path.begin(); it != path.end() - 1; ++it) {
+    DistanceResult dr =
+        _nodes.at(*it)->getDistancetoNeighbour(_nodes.at(*(it + 1)));
+    if (!dr.valid) {
+      pathLengthResult.valid = false;
+      pathLengthResult.distance = -1;
+    }
+    if (pathLengthResult.valid) {
+      pathLengthResult.distance += dr.distance;
+    }
+  }
+  return pathLengthResult.distance;
+}
+
+std::vector<NodeIndex> navi(const NodeIndex i, const NodeIndex j) {}
--- graph.hpp
+++ graph.hpp
@@ -1,9 +1,14 @@
-#ifndef GALEYSHAPLEY_H
-#define GALEYSHAPLEY_H
+#ifndef GRAPH_H
+#define GRAPH_H
 
 #include "node.hpp"
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
+
+using NodeIndex = unsigned int;
 
 class Graph {
 public:
@@ -13,7 +18,7 @@ public:
   Graph(const std::string &filename);
 
   //! Copy constructor
-  Graph(const Graph &other);
+  Graph(const Graph &other) = delete;
 
   //! Move constructor
   Graph(Graph &&other) noexcept;
@@ -27,9 +32,22 @@ public:
   //! Move assignment operator
   Graph &operator=(Graph &&other) noexcept;
 
+  double edgeLength(const NodeIndex i, const NodeIndex j);
+
+  double pathLength(const std::vector<NodeIndex> &path);
+
+  std::vector<NodeIndex> navi(const NodeIndex i, const NodeIndex j);
+
 protected:
 private:
-  std::vector<Node *> _nodes;
+  std::map<NodeIndex, NodePtr> _nodes;
+  DistanceResult _edgeLength(const NodeIndex i, const NodeIndex j);
 };
 
-#endif // GALEYSHAPLEY_H
+class DijkstraNode {
+public:
+  double distance;
+  NodePtr predecessor;
+}
+
+#endif // GRAPH_H
