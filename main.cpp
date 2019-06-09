6f71b6e9ebffcb01fdac135920917b4381363b9d
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2019-06-09 17:04:56 +0200

    Enabled path 2 and 3

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main() {
   Graph graph(filename);
 
   auto path1 = graph.navi(54, 56);
-  // auto path2 = graph.navi(32, 9);
-  // auto path3 = graph.navi(23, 8);
+  auto path2 = graph.navi(32, 9);
+  auto path3 = graph.navi(23, 8);
 
   std::cout << "Shortest Path between 54 & 56:\n";
   for (auto &node : path1) {
@@ -19,17 +19,17 @@ int main() {
   }
   std::cout << "\n";
 
-  // std::cout << "Shortest Path between 32 & 9:\n";
-  // for (auto &node : path2) {
-  //   std::cout << node << " ";
-  // }
-  // std::cout << "\n";
+  std::cout << "Shortest Path between 32 & 9:\n";
+  for (auto &node : path2) {
+    std::cout << node << " ";
+  }
+  std::cout << "\n";
 
-  // std::cout << "Shortest Path between 23 & 8:\n";
-  // for (auto &node : path3) {
-  //   std::cout << node << " ";
-  // }
-  // std::cout << "\n" << endl;
+  std::cout << "Shortest Path between 23 & 8:\n";
+  for (auto &node : path3) {
+    std::cout << node << " ";
+  }
+  std::cout << "\n" << endl;
 
   return 0;
 }
